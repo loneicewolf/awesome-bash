f04da08f4301fb2b1aeac90e400114dbfa1163f6
somebody <68499986+loneicewolf@users.noreply.github.com>
2020-10-10 12:34:39 +0200

    Update Custom_SUDO_Prompt.sh

--- Custom_SUDO_Prompt.sh
+++ Custom_SUDO_Prompt.sh
@@ -1,18 +1,18 @@
 ## ****Things in Bash you *maybe* didn't know**** ##
 
 # Customize sudo - Password Prompt: 
-$ export SUDO_PROMPT="Hello, please type in your magic word: "
-$ sudo su
+export SUDO_PROMPT="Hello, please type in your magic word: "
+sudo su
 
 # output: #
-Hello, please type in your magic word: 
+# Hello, please type in your magic word: 
 
 
 # To Go back to Default prompt:
-$ unset SUDO_PROMPT
+unset SUDO_PROMPT
 
 
 # Another way, more 1-time like:
-$ sudo -p "Custom prompt:" ls
+sudo -p "Custom prompt:" ls
 #output
-Custom prompt:
+# Custom prompt:
